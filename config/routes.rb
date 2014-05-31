a37da33b48daf0b865ebb44029a8798aeb66e7b6
Derrick Wolbert <wolbert.derrick@gmail.com>
2014-05-31 17:16:28 -0400

    built parent

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 MasterfulApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root  'static_pages#home'
+  match '/signup',  to: 'parents#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :children
 
   resources :parents
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531174149) do
+ActiveRecord::Schema.define(version: 20140531211352) do
 
   create_table "children", force: true do |t|
     t.string   "content"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20140531174149) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "parents", ["email"], name: "index_parents_on_email", unique: true
 
 end
